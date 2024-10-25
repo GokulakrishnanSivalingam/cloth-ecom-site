1e84241b152d2254562d845694658fba765f344e
1234gk <144155567+1234gk@users.noreply.github.com>
2024-10-25 08:04:40 +0530

    changes

--- src/main.jsx
+++
@@ -1,8 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Index from './Index.jsx'
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Index />
-  </StrictMode>,
-)
